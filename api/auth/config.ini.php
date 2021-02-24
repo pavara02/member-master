26e8ec6e6adc618ff8174a75b42bb2fa86aaae48
Pavara Silpipat <progameai@gmail.com>
2021-02-24 10:50:09 +0700

    test
    test

---
+++ api/auth/config.ini.php
@@ -0,0 +1,3 @@
+<?php
+define("API_TOKEN", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2MTM2MjkyMDAsInVpZCI6Imh0dHBzOlwvXC9hcHBsZWdvbGQ5OS5jb20iLCJpc3MiOiJodHRwczpcL1wvYXBwbGVnb2xkOTkuY29tIn0.z8tX57IL8vRSqPqu5gHca-Xs3iFWn409r8KcemmRpqs"); // API Token
+define("API_SERVER", "https://agentapplegold99.com/member/api/v1.0"); //API Server 
